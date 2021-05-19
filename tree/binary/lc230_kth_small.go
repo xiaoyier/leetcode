2f2a43a46c4f88fc6dada5d44e8fcb04c1d74cc7
Sycamor <914359463@qq.com>
2021-05-20 01:19:05 +0800

    add: 230, 235

---
+++ tree/binary/lc230_kth_small.go
@@ -0,0 +1,28 @@
+package binary
+
+
+// 给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 个最小元素（从 1 开始计数）。
+
+// https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/
+
+func kthSmallest(root *TreeNode, k int) int {
+	index := 0
+	result := -1
+	var dfs func(node *TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil || result >= 0 {
+			return
+		}
+
+		dfs(node.Left)
+		index++
+		if index == k {
+			result = node.Val
+			return
+		}
+		dfs(node.Right)
+	}
+
+	dfs(root)
+	return result
+}
