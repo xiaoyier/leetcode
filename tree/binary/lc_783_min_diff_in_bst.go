189010fe6068d9846bcd5cef699174605ad85455
Sycamor <914359463@qq.com>
2021-05-19 22:32:58 +0800

    add: 783

---
+++ tree/binary/lc_783_min_diff_in_bst.go
@@ -0,0 +1,28 @@
+package binary
+
+// 给你一个二叉搜索树的根节点 root ，返回 树中任意两不同节点值之间的最小差值 。
+
+// https://leetcode-cn.com/problems/minimum-distance-between-bst-nodes/
+
+func minDiffInBST(root *TreeNode) int {
+	diff := 1 << 63 - 1
+	pre := -1
+	stack := []*TreeNode{}
+	for len(stack) > 0 || root != nil {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+
+		root = stack[len(stack) - 1]
+		stack = stack[:len(stack) - 1]
+
+		if pre != -1 && diff > root.Val - pre {
+			diff = root.Val - pre
+		}
+		pre = root.Val
+		root = root.Right
+	}
+
+	return diff
+}
