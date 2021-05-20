5f14c1c655cc791ca9e701df67d645e8007b84d9
Sycamor <914359463@qq.com>
2021-05-20 18:15:06 +0800

    add: add 99

---
+++ tree/binary/lc99_recover_bst.go
@@ -0,0 +1,53 @@
+package binary
+
+//给你二叉搜索树的根节点 root ，该树中的两个节点被错误地交换。请在不改变其结构的情况下，恢复这棵树。
+//
+//进阶：使用 O(n) 空间复杂度的解法很容易实现。你能想出一个只使用常数空间的解决方案吗？
+
+// https://leetcode-cn.com/problems/recover-binary-search-tree/
+
+func recoverTree(root *TreeNode)  {
+	var node1, node2 *TreeNode
+	preNode := root
+	stack := []*TreeNode{}
+	for len(stack) > 0 || root != nil {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+
+		root = stack[len(stack) - 1]
+		stack = stack[:len(stack)-1]
+		if preNode != nil && preNode.Val > root.Val {
+			if node1 == nil {
+				node1 = preNode
+				node2 = root
+			} else {
+				node2 = root
+			}
+		}
+		preNode = root
+		root = root.Right
+	}
+
+	if node1 == nil || node2 == nil {
+		return
+	}
+	node1.Val, node2.Val = node2.Val, node1.Val
+
+	//
+	//queue := []*TreeNode{root}
+	//if len(queue) > 0 {
+	//	node := queue[0]
+	//	queue = queue[1:]
+	//
+	//	if node.Left != nil && node.Left.Val > node.Val {
+	//		node.Val = node.Left.Val
+	//		queue = append(queue, node.Left)
+	//	}
+	//	if node.Right != nil && node.Right.Val < node.Val {
+	//		node.Val = node.Right.Val
+	//		queue = append(queue, node.Right)
+	//	}
+	//}
+}
