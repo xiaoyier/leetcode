70366e3b14e7e990e185287724e2c773943fc596
Sycamor <914359463@qq.com>
2021-05-21 00:41:43 +0800

    add: add 102

---
+++ tree/binary/lc102_levelorder_traversal.go
@@ -0,0 +1,35 @@
+package binary
+
+// 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+
+// https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
+
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+	q := []*TreeNode{root}
+	result := [][]int{}
+	level := []int{}
+	size := 0
+	for len(q) > 0 {
+		node := q[0]
+		q = q[1:]
+
+		if node.Left != nil {
+			q = append(q, node.Left)
+		}
+		if node.Right != nil {
+			q = append(q, node.Right)
+		}
+
+		level = append(level, node.Val)
+		size--
+		if size == 0 {
+			result = append(result, level)
+			level = []int{}
+			size = len(q)
+		}
+	}
+	return result
+}
